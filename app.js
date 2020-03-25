b9c33e208ddd22026332c4257c24b3d5be87f902
Shyaboi <ian.shane.sears@gmail.com>
2020-03-25 09:43:41 -0700

    re-setup, added schema and seed sql and setup connection

--- app.js
+++ app.js
@@ -1,84 +1,23 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const mysql = require("mysql");
+// var table = require('console.table');
+const inquirer = require('inquirer');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const connection = mysql.createConnection({
+    host: "localhost",
 
-const render = require("./lib/htmlRenderer");
+    // Your port;
+    port: 3306,
 
-// console.log(render);
-// render(dave)
+    // Your username
+    user: "root",
 
-function start() {
-  inquirer
-    .prompt([
-        {
-      type: "list",
-      message: "What type of employee would you like to add?",
-      name: "role",
-      choices: [
-        "Manager",
-        "Engineer",
-        "Intern",
-        "I'm finished adding employees"
-      ]
-    },
+    password: "",
+    database: "employees_db"
+});
 
-],
-    
-    )
-    .then(function(response) {
-    //   console.log(response);
-    //   console.log(response.role);
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
 
-      switch (response.role) {
-        case "Manger":
-          manager(new Manager(djkn,mkflds,nmjkfnsd));
-          console.log(role);
-          break;
-        case "Engineer":
-          engineer();
-          break;
-        case "Intern":
-          intern();
-          break;
-        default:
-          finish();
-      }
-    });
-}
-start();
-
-function manager() {
-}
-function intern() {}
-
-function engineer() {}
-
-function finish() {}
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
---
+++ seeds.sql
@@ -0,0 +1,40 @@
+USE employees_db;
+
+INSERT INTO department (name)
+VALUES ("Top Scams");
+INSERT INTO department (name)
+VALUES ("Product Mongers");
+INSERT INTO department (name)
+VALUES ("Money People");
+INSERT INTO department (name)
+VALUES ("The Rest");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Top Lad", 3000490, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Grand Master of Underlings", 534805304, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Dream Alchemist", 4389439, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Bank Robber", 4378295, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Cheif Everything Officer", 4993000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bill", "Murphy", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Glen", "Stephanie", 2, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Hank", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Billy", "Crystals", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tyler", "Swift", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Beyondce", "Moles", 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bobert", "Downy Sr.", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jorge", "Cloney", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Opera", "Windyfree", 3, 7);
